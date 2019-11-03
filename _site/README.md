a80a074841c2c3db6fc66badb6ed1c3ba442f52f
oxide8689 <etemenanki@gmail.com>
2019-11-03 13:27:00 +0900

    commit.

---
+++ _site/README.md
@@ -0,0 +1,1 @@
+# esperancer.github.io
---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome To Jekyll",
+        "excerpt":"안녕하세요! 지킬(Jekyll) 을 이용해 세팅해본 블로그의 첫 포스팅입니다.기본적으로 Github의 마크다운 문법을 지원합니다. 막상 설치해보니 생각만큼 세팅이 잘 안되는 부분도 있어서 하나하나 고쳐나가야할 것 같네요: ) 추가하고자 하는 기능들   댓글 기능  프로필 작성  포스팅 주제 선정 등..","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/welcome-to-jekyll/"
+      }]
