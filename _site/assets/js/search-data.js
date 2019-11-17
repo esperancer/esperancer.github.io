899ba96c79a2d5235a6ff5110b1754daa3055c79
oxide8689 <etemenanki@gmail.com>
2019-11-17 10:36:55 +0900

    commit.

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"안녕하세요! 지킬(Jekyll) 을 이용해 세팅해본 블로그의 첫 포스팅입니다.기본적으로 Github의 마크다운 문법을 지원합니다. 막상 설치해보니 생각만큼 세팅이 잘 안되는 부분도 있어서 하나하나 고쳐나가야할 것 같네요: ) 추가하고자 하는 기능들   태그 기능  댓글 기능  프로필 작성  포스팅 주제 선정 등..","categories": [],
         "tags": ["Jekyll","블로그","Github"],
         "url": "http://localhost:4000/welcome-to-jekyll/"
+      },{
+        "title": "KSUG 하반기 세미나 후기",
+        "excerpt":"글 작성 중.. ","categories": [],
+        "tags": ["KSUG","한국스프링사용자모임","Java","MSA","Monolithic"],
+        "url": "http://localhost:4000/KSUG-%ED%95%98%EB%B0%98%EA%B8%B0-%EC%84%B8%EB%AF%B8%EB%82%98-%ED%9B%84%EA%B8%B0/"
       }]
